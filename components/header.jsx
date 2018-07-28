145e071d51051bfee4687eb22c73642b655e3310
theRegex <keinan.jpace@gmail.com>
2018-07-28 16:58:56 -0500

    navigation update

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
 
-	 export default class Header extends React.Component{
+	export default class Header extends React.Component {
 		state = {}
 		render(){
-			
-			return <div className="header">
-			<h1>Manage Products</h1>
-		  </div>
+				return <div className="header"></div>
 		}
 	}
--- components/navigation.jsx
+++ components/navigation.jsx
@@ -7,16 +7,16 @@ import { Link } from 'react-router-dom';
 		render(){
 			return <div className="navigation_bar">
 			<div className="navbarHeader">
-				<h1 className="navbarTitle">Bosseta</h1>
+				<h1 className="navbarTitle">Site Title</h1>
 			  <br/>
 			  <h5 className="navSectionTitle">
 				 Navigation
 			  </h5>
 			  <ul className="navList">
-				<li className="navItem"><i className="fas fa-box"></i><Link to="/products">Creat Product</Link></li>
-				 <li className="navItem"><i className="fas fa-grip-horizontal"></i><Link to="/categories">Categories</Link></li>
-				 <li className="navItem"><i className="fas fa-th-list"></i><Link to="/collections">Collections</Link></li>
-				 <li className="navItem"><i className="fas fa-clipboard-list"></i><Link to="/">Manage Prducts</Link></li>
+				<li className="navItem"><i className="fas fa-box"></i><Link to="/products">Create Product</Link></li>
+				 <li className="navItem"><i className="fas fa-grip-horizontal"></i><Link to="/categories">Create Category</Link></li>
+				 <li className="navItem"><i className="fas fa-th-list"></i><Link to="/collections">Create Collection</Link></li>
+				 <li className="navItem"><i className="fas fa-clipboard-list"></i><Link to="/">Manage Products</Link></li>
 				 <li className="navItem"><i className="fas fa-folder-open"></i><Link to="/orders">Manage Orders</Link></li>
 				 <li className="navItem"><i className="fas fa-truck-moving"></i><Link to="/tracking">Track Order</Link></li>
 				<li className="navItem"><i className="fas fa-cog"></i><Link to="/settings">Settings</Link></li>
